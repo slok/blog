1458f6419bcc9d293896050c68732a4adfb6d3b7
Xabier Larrakoetxea <slok69@gmail.com>
2012-10-15 14:05:03 +0200

    Added settings template

---
+++ conf/settings.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+AUTHOR =
+SITENAME =
+SITEURL =
+TIMEZONE =
+
+GITHUB_URL =
+DISQUS_SITENAME =
+PDF_GENERATOR =
+REVERSE_CATEGORY_ORDER =
+LOCALE =
+DEFAULT_PAGINATION =
+
+THEME =
+
+FEED_RSS =
+CATEGORY_FEED_RSS =
+
+LINKS =
+
+SOCIAL =
+
+OUTPUT_PATH =
+PATH =
+
+# global metadata to all the contents
+DEFAULT_METADATA =
+
+# static paths will be copied under the same name
+STATIC_PATHS =
+
+# A list of files to copy from the source to the destination
+FILES_TO_COPY =
